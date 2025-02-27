9b4cf8dfb0b9cc9125361e3f892ce5b4d972a623
Mohammad Sameer <sameer007nrg@gmail.com>
2025-02-27 14:35:53 +0530

    Day 24  added

--- Day 24/Email merge/main.py
+++ Day 24/Email merge/main.py
@@ -6,3 +6,17 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open("Input/Names/invited_names.txt") as names_file:
+    names = names_file.readlines()
+
+with open("Input/Letters/starting_letter.txt") as format_file:
+    format= format_file.read()
+    for name in names:
+        new_file = format.replace("[name]", name.strip())
+        with open(f"Output/ReadyToSend/letter_for_{name.strip()}.txt", mode="w") as completed_file:
+            completed_file.write(new_file)
+
+
+
+
